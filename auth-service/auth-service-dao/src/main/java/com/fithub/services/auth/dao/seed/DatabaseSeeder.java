969ebcc0d9eefbe04c4b0ee8f28dac2db798dbd4
Matija Kokor <matijakokorr@gmail.com>
2024-05-29 22:16:12 +0200

    image paths changed

--- auth-service/auth-service-dao/src/main/java/com/fithub/services/auth/dao/seed/DatabaseSeeder.java
+++ auth-service/auth-service-dao/src/main/java/com/fithub/services/auth/dao/seed/DatabaseSeeder.java
@@ -43,7 +43,7 @@ public class DatabaseSeeder implements ApplicationRunner {
             coachJohnEntity.setBiography(
                     "John Doe is a dedicated and experienced fitness trainer with over a decade of experience in helping clients achieve their health and fitness goals. With certifications from the National Academy of Sports Medicine (NASM) and the National Strength and Conditioning Association (NSCA), John combines his extensive knowledge with a passion for fitness to create personalized, effective, and safe training programs.");
             coachJohnEntity.setClientCapacity(3);
-            coachJohnEntity.setImagePath("/fithub-auth-service/images/coach/john-doe-coach.jpg");
+            coachJohnEntity.setImagePath("/auth-service/images/coach/john-doe-coach.jpg");
             coachJohnEntity.setUser(coachJohnUserEntity);
 
             coachRepository.save(coachJohnEntity);
@@ -65,7 +65,7 @@ public class DatabaseSeeder implements ApplicationRunner {
             coachMaryEntity.setBiography(
                     "Mary Ann is a passionate and experienced fitness trainer dedicated to helping clients achieve their health and fitness goals. With over a decade of experience in the industry, she combines her deep knowledge of exercise science with a motivational coaching style that inspires clients to push beyond their limits. Mary Ann specializes in weight loss, strength training, and functional fitness, creating personalized programs that are both effective and enjoyable. Her holistic approach to fitness ensures clients not only reach their physical goals but also improve their overall well-being. Mary Ann's supportive nature and expertise have made her a trusted guide for many on their fitness journeys.");
             coachMaryEntity.setClientCapacity(3);
-            coachMaryEntity.setImagePath("/fithub-auth-service/images/coach/mary-ann-coach.jpg");
+            coachMaryEntity.setImagePath("/auth-service/images/coach/mary-ann-coach.jpg");
             coachMaryEntity.setUser(coachMaryUserEntity);
 
             coachRepository.save(coachMaryEntity);
@@ -87,7 +87,7 @@ public class DatabaseSeeder implements ApplicationRunner {
             coachJamesEntity.setBiography(
                     "James Martin is a dedicated and experienced fitness trainer with a passion for helping clients achieve their health and fitness goals. With over a decade in the industry, James has developed a reputation for his personalized approach and motivational coaching style. He specializes in weight loss, strength training, and functional fitness, ensuring that each program is tailored to the individual needs of his clients. James believes in a holistic approach to fitness, focusing on physical health, proper nutrition, and mental well-being. His supportive nature and expertise have helped many clients transform their lives and achieve lasting results.");
             coachJamesEntity.setClientCapacity(20);
-            coachJamesEntity.setImagePath("/fithub-auth-service/images/coach/james-martin-coach.jpg");
+            coachJamesEntity.setImagePath("/auth-service/images/coach/james-martin-coach.jpg");
             coachJamesEntity.setUser(coachJamesUserEntity);
 
             coachRepository.save(coachJamesEntity);
