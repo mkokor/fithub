46ef126ebd7a8dfe98965a25e5e0a02d819163a7
Nikolina Kokor <nkokor@icloud.com>
2024-06-10 21:50:12 +0200

    generic function for request sending implemented

---
+++ fithub-fe/src/api/ChatApi.js
@@ -0,0 +1,59 @@
+import { sendRequest } from './GenericApi.js'; 
+
+export const getChatroomData = async () => {
+  try {
+    const request = {
+      url: '/chat-service/chatroom/data',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const response = await sendRequest(request);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching chatroom data:', error);
+    return { "chatroomDetails": "", "messages": [], "participants": [] };
+  }
+};
+
+export const getChatroomMessages = async () => {
+  try {
+    const request = {
+      url: '/chat-service/message',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const response = await sendRequest(request);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching chatroom messages:', error);
+    return { "messages": [] };
+  }
+};
+
+export const sendNewMessage = async (messageData) => {
+  try {
+    const request = {
+      url: '/chat-service/message/sendMessage',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(messageData)
+    };
+    const response = await sendRequest(request);
+    return await response.json();
+  } catch (error) {
+    console.error('Error sending message:', error);
+    return { 
+      "id": "",
+      "chatroomId": "",
+      "username": "",
+      "content": "",
+      "created": ""
+    };
+  }
+};
