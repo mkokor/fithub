7778d393b8ad7e80ced1ff37fa04a532c30aa533
Nikolina Kokor <nkokor@icloud.com>
2024-06-11 23:18:40 +0200

    navigation bubbles on home page implemented

--- fithub-fe/src/components/Home.js
+++ fithub-fe/src/components/Home.js
@@ -36,9 +36,7 @@ const Home = () => {
     <div id="home-page-div">
       <HomeNewsSlider news={news}></HomeNewsSlider>
       <p id='home-text' className="text-p">Welcome to FitHub - your personal path to better health</p>
-      <p className="text-p">Here at FitHub our goal is to give you all the tools and support you need in order to reach your goals.</p>      
-      <p className="text-p">Our app is designed to save you time and make your organization process as easy as possible.</p>
-      <p className="text-p"> Use our inutitive interface to book training appointments that fit your personal schedule, catch up on your latest meal plan or use our real-time chat platform to connect to your coach and teammates.</p> 
+      <p className="text-p">Here at FitHub our goal is to give you all the tools and support you need in order to reach your goals. Our app is designed to save you time and make your organization process as easy as possible. Use our inutitive interface to book training appointments that fit your personal schedule, catch up on your latest meal plan or use our real-time chat platform to connect to your coach and teammates.</p>      
       <Options></Options>
       <Footer></Footer>
     </div>
--- fithub-fe/src/components/Options.js
+++ fithub-fe/src/components/Options.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import '../css/Options.css'; 
+import { useNavigate } from 'react-router-dom';
+import '../css/Options.css';
 
 const Options = () => {
+  const navigate = useNavigate();
 
   return (
     <div className="workout-styles-container">
-      <div className="image-wrapper">
-        <img src="/images/training.jpg" alt={`WEIGHT TRAINING`} className="workout-image" />
-        <p className='workout-style-p'>TRAINING</p>
+      <div className="image-wrapper" onClick={() => navigate('/training')}>
+        <img src="/images/training.jpg" alt="WEIGHT TRAINING" className="workout-image" />
+        <p className="workout-style-p">TRAINING</p>
       </div>
-      <div className="image-wrapper">
-        <img src="/images/nutrition.jpg" alt={`YOGA`} className="workout-image" />
-        <p className='workout-style-p'>NUTRITION</p>
+      <div className="image-wrapper" onClick={() => navigate('/nutrition')}>
+        <img src="/images/nutrition.jpg" alt="NUTRITION" className="workout-image" />
+        <p className="workout-style-p">NUTRITION</p>
       </div>
-      <div className="image-wrapper">
-        <img src="/images/chat.jpg" alt={`PILATES`} className="workout-image" />
-        <p className='workout-style-p'>CHAT</p>
+      <div className="image-wrapper" onClick={() => navigate('/chatroom')}>
+        <img src="/images/chat.jpg" alt="CHAT" className="workout-image" />
+        <p className="workout-style-p">CHAT</p>
       </div>
     </div>
   );
