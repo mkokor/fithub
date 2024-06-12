7876de5701f4c108bf32b83c08abbfaf0d943585
Nedzla Helac <nhelac1@etf.unsa.ba>
2024-06-12 20:36:40 +0200

    full appointment

--- fithub-fe/src/components/modals/AppointmentsDetailsModal.js
+++ fithub-fe/src/components/modals/AppointmentsDetailsModal.js
@@ -9,7 +9,12 @@ const AppointmentsDetailsModal = ({ closeModal, selectedTermin }) => {
         <h2>Details about Appointments</h2>
         <p><strong>Day:</strong> {selectedTermin.day}</p>
         <p><strong>Time:</strong> {selectedTermin.startTime} - {selectedTermin.endTime}</p>
-        <p><strong>Capacity:</strong> {selectedTermin.capacity}</p>
+        <p>
+          <strong>Capacity:</strong> {selectedTermin.capacity}{" "}
+          {selectedTermin.capacity === 10 && (
+            <span className="full-termin">(full appointment)</span>
+          )}
+        </p>
         <div className="button-container">
           <button onClick={closeModal}>Close</button>
         </div>
