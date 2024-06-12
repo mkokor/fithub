449cf4eae5881f993b9ca48c21796647d048bce5
Matija Kokor <matijakokorr@gmail.com>
2024-06-12 19:31:46 +0200

    implemented progression stats endpoints

--- training-service/training-service-dao/src/main/java/com/fithub/services/training/dao/model/ProgressionStatsEntity.java
+++ training-service/training-service-dao/src/main/java/com/fithub/services/training/dao/model/ProgressionStatsEntity.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -37,9 +38,28 @@ public class ProgressionStatsEntity {
     @NotNull(message = "The client must be specified.")
     private ClientEntity client;
 
+    @NotNull(message = "The weigth must be specified.")
+    @Min(value = 0, message = "The wight must not be negative.")
     private Double weight;
 
-    @JoinColumn(name = "pr_stats")
-    private String prStats;
+    @NotNull(message = "The height must be specified.")
+    @Min(value = 0, message = "The height PR value must not be negative.")
+    private Double height;
+
+    @JoinColumn(name = "deadlift_pr")
+    @Min(value = 0, message = "The deadlift PR value must not be negative.")
+    private Double deadliftPr;
+
+    @JoinColumn(name = "squat_pr")
+    @Min(value = 0, message = "The squat PR value must not be negative.")
+    private Double squatPr;
+
+    @JoinColumn(name = "bench_pr")
+    @Min(value = 0, message = "The bench PR value must not be negative.")
+    private Double benchPr;
+
+    @JoinColumn(name = "treadmill_pr")
+    @Min(value = 0, message = "The treadmill PR value must not be negative.")
+    private Double treadmillPr;
 
 }
---
+++ training-service/training-service-rest/src/main/java/com/fithub/services/training/rest/progressionstats/ProgressionStatsController.java
@@ -0,0 +1,69 @@
+package com.fithub.services.training.rest.progressionstats;
+
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fithub.services.training.api.ProgressionStatsService;
+import com.fithub.services.training.api.exception.ApiException;
+import com.fithub.services.training.api.model.progressionstat.ProgressionStatsCreateRequest;
+import com.fithub.services.training.api.model.progressionstat.ProgressionStatsResponse;
+import com.fithub.services.training.api.model.validation.ProgressionStatsSort;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "progression-stats", description = "Progression Stats API")
+@RestController
+@RequestMapping(value = "progression-stats", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequiredArgsConstructor
+public class ProgressionStatsController {
+
+    private final ProgressionStatsService progressionStatsService;
+
+    @Operation(summary = "Create progression stats")
+    @PostMapping
+    public ResponseEntity<ProgressionStatsResponse> crateProgressionStats(
+            @Valid @RequestBody ProgressionStatsCreateRequest progressionStatsCreateRequest) throws Exception {
+        return new ResponseEntity<>(progressionStatsService.create(progressionStatsCreateRequest), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get progression stats for client")
+    @GetMapping("/client/{uuid}")
+    public ResponseEntity<List<ProgressionStatsResponse>> getProgressionStatsForClient(@PathVariable final String uuid)
+            throws ApiException {
+        return new ResponseEntity<>(progressionStatsService.getProgressionStats(uuid), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get latest progression stats for authenticated client")
+    @GetMapping("/latest")
+    public ResponseEntity<ProgressionStatsResponse> getLatestProgressionStats() throws ApiException {
+        return new ResponseEntity<>(progressionStatsService.getLatestStats(), HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get progression stats rang list")
+    @GetMapping("/rang-list")
+    public ResponseEntity<List<ProgressionStatsResponse>> getRangList(
+            @RequestParam(name = "page_number", required = false, defaultValue = "0") final Integer pageNumber,
+            @RequestParam(name = "page_size", required = false, defaultValue = "5") final Integer pageSize,
+            @RequestParam(name = "sort_by", required = false, defaultValue = "createdAt") @ProgressionStatsSort final String sortBy)
+            throws ApiException {
+        PageRequest pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc("createdAt"), Sort.Order.desc(sortBy)));
+
+        return new ResponseEntity<>(progressionStatsService.getRangList(pageable), HttpStatus.OK);
+    }
+
+}
